402b0f7c3beddd5ee3ae0e7ac937f60d62651c63
Palash Bansal <PalashBansal96@gmail.com>
2017-11-01 05:07:17 +0530

    Payments model and the date fields.

--- SecureBankingSystem/BankingSystem/models.py
+++ SecureBankingSystem/BankingSystem/models.py
@@ -4,24 +4,26 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
 
-
 # Create your models here.
 from django.db.models import SET_NULL, CASCADE
 
 
 class Profile(models.Model):
 	user = models.OneToOneField(User, unique=True, editable=False, on_delete=CASCADE, primary_key=True)
-	aadhar = models.CharField(max_length=30)
 	phone = models.CharField(max_length=13)
 	address = models.CharField(max_length=200)
 	is_employee = models.BooleanField(default=False)
 	manager = models.ForeignKey("self", null=True, default=None, on_delete=SET_NULL)
+	creation_time = models.DateTimeField(auto_now_add=True)
+	last_changed_time = models.DateTimeField(auto_now=True)
 
 
 class Account(models.Model):
 	user = models.ForeignKey(Profile, editable=False, null=True, on_delete=SET_NULL)
 	number = models.IntegerField(primary_key=True, unique=True, editable=False)
 	balance = models.IntegerField(default=0)
+	creation_time = models.DateTimeField(auto_now_add=True)
+	last_changed_time = models.DateTimeField(auto_now=True)
 
 
 class Transactions(models.Model):
@@ -37,3 +39,15 @@ class Transactions(models.Model):
 	amount = models.IntegerField(default=0)
 	status = models.CharField(max_length=1, choices=STATUS)
 	is_cash = models.BooleanField()
+	creation_time = models.DateTimeField(auto_now_add=True)
+	last_changed_time = models.DateTimeField(auto_now=True)
+
+
+class Payments(models.Model):
+	target_account = models.ForeignKey(Account)
+	target_user = models.ForeignKey(Profile)
+	transaction = models.ForeignKey(Transactions, null=True, default=None)
+	creation_time = models.DateTimeField(auto_now_add=True)
+	last_changed_time = models.DateTimeField(auto_now=True)
+	is_done = models.BooleanField(default=False)
+
--- SecureBankingSystem/BankingSystem/views.py
+++ SecureBankingSystem/BankingSystem/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 # Create your views here.

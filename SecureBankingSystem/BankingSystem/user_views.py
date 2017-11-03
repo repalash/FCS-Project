e36827cfd7c87781966808ee3c234b63ca161370
nishantsinha15 <nishant15066@iiitd.ac.in>
2017-11-03 11:10:16 +0530

    changed debit_credit() in views.py

--- SecureBankingSystem/BankingSystem/user_views.py
+++ SecureBankingSystem/BankingSystem/user_views.py
@@ -9,17 +9,17 @@ from BankingSystem.utils import do_get, custom_redirect, BankingParseException
 @login_required()
 @permission_required('BankingSystem.user_operations', raise_exception=True)
 def dashboard_external(request):
-	fields = {
-		'username': request.user.username,
-		'redirect_info': do_get(request.GET, 'info'),  # Like already logged in
-		'redirect_success': do_get(request.GET, 'success'),  # Like login successful
-		'redirect_error': do_get(request.GET, 'error'),  # Generic site error
-		'error': '',
-		'has_perm_user_operations': request.user.has_perm('BankingSystem.user_operations'),
-		'has_perm_create_payments': request.user.has_perm('BankingSystem.create_payments'),  # check if user is Company
-	}
+    fields = {
+        'username': request.user.username,
+        'redirect_info': do_get(request.GET, 'info'),  # Like already logged in
+        'redirect_success': do_get(request.GET, 'success'),  # Like login successful
+        'redirect_error': do_get(request.GET, 'error'),  # Generic site error
+        'error': '',
+        'has_perm_user_operations': request.user.has_perm('BankingSystem.user_operations'),
+        'has_perm_create_payments': request.user.has_perm('BankingSystem.create_payments'),  # check if user is Company
+    }
 
-	return render(request, 'dashboard_external_user.html', fields)
+    return render(request, 'dashboard_external_user.html', fields)
 
 
 # Transfer money from one account to another
@@ -30,75 +30,75 @@ def dashboard_external(request):
 @login_required()
 @permission_required('BankingSystem.user_operations', raise_exception=True)
 def make_transactions(request):
-	fields = {
-		'username': request.user.username,
-		'error': '',
-		'has_perm_view_critical_transactions': request.user.has_perm('BankingSystem.view_critical_transactions'),
-		'has_perm_create_payments': request.user.has_perm('BankingSystem.create_payments'),
-		'accounts': request.user.profile.account_set.all()
-	}
-	if request.method != 'POST':
-		return render(request, 'make_transactions.html', fields)
-	sender_account_number = request.POST['sender_account_number']
-	receiver_account_number = request.POST['receiver_account_number']
-	amount = request.POST['amount']
-	try:
-		transaction = Transactions.create(Transactions.TYPE_TRANSACTION, request.user, sender_account_number,
-		                                  receiver_account_number, amount)
-	except BankingParseException as e:
-		fields['error'] = e.message
-		return render(request, 'make_transactions.html', fields)
-	return redirect("transaction_confirmation", transaction_id=transaction.id)
+    fields = {
+        'username': request.user.username,
+        'error': '',
+        'has_perm_view_critical_transactions': request.user.has_perm('BankingSystem.view_critical_transactions'),
+        'has_perm_create_payments': request.user.has_perm('BankingSystem.create_payments'),
+        'accounts': request.user.profile.account_set.all()
+    }
+    if request.method != 'POST':
+        return render(request, 'make_transactions.html', fields)
+    sender_account_number = request.POST['sender_account_number']
+    receiver_account_number = request.POST['receiver_account_number']
+    amount = request.POST['amount']
+    try:
+        transaction = Transactions.create(Transactions.TYPE_TRANSACTION, request.user, sender_account_number,
+                                          receiver_account_number, amount)
+    except BankingParseException as e:
+        fields['error'] = e.message
+        return render(request, 'make_transactions.html', fields)
+    return redirect("transaction_confirmation", transaction_id=transaction.id)
 
 
 # Get OTP from the user, verifies and sends transaction for approval
 # TODO team: No need for password - Done. Removed from the page
 # TODO palash: Implement OTP
 def transaction_confirmation(request, transaction_id):  # done
-	fields = {
-		'authentication_error': '',
-		'username': request.user.username,
-		'error': '',
-		'has_perm_user_operations': request.user.has_perm('BankingSystem.view_user_operations'),
-		'has_perm_create_payments': request.user.has_perm('BankingSystem.create_payments'),
-	}
+    fields = {
+        'authentication_error': '',
+        'username': request.user.username,
+        'error': '',
+        'has_perm_user_operations': request.user.has_perm('BankingSystem.view_user_operations'),
+        'has_perm_create_payments': request.user.has_perm('BankingSystem.create_payments'),
+    }
 
-	if request.method != 'POST':
-		return render(request, 'transaction_confirmation_otp.html', fields)
+    if request.method != 'POST':
+        return render(request, 'transaction_confirmation_otp.html', fields)
 
-	otp = request.POST['otp']
-	transaction = Transactions.objects.get(pk=transaction_id)
-	try:
-		transaction.verify_otp(otp)
-	except BankingParseException as e:
-		fields['authentication_error'] = e.message
-		return render(request, 'transaction_confirmation_otp.html', fields)
-	return custom_redirect("dashboard", success='Transaction sent for approval')
+    otp = request.POST['otp']
+    transaction = Transactions.objects.get(pk=transaction_id)
+    try:
+        transaction.verify_otp(otp)
+    except BankingParseException as e:
+        fields['authentication_error'] = e.message
+        return render(request, 'transaction_confirmation_otp.html', fields)
+    return custom_redirect("dashboard", success='Transaction sent for approval')
 
 
 # TODO palash: ...
 @login_required()
 @permission_required('BankingSystem.user_operations', raise_exception=True)
 def edit_user_details(request):
-	fields = {
-		'username': request.user.username,
-		'address': request.user.address,
-		'name': request.user.name,
-		'phone': request.user.phone,
-		'error': '',
-		'is_error': False,
-	}
-	if request.method != 'POST':
-		return render(request, 'edit_user_details.html', fields)
+    fields = {
+        'username': request.user.username,
+        'address': request.user.address,
+        'name': request.user.name,
+        'phone': request.user.phone,
+        'error': '',
+        'is_error': False,
+    }
+    if request.method != 'POST':
+        return render(request, 'edit_user_details.html', fields)
 
-	username = request.POST['username']
-	password = request.POST['password']
-	repeat_password = request.POST['repeat_password']
-	name = request.POST['name']
-	address = request.POST['address']
-	phone = request.POST['phone']
+    username = request.POST['username']
+    password = request.POST['password']
+    repeat_password = request.POST['repeat_password']
+    name = request.POST['name']
+    address = request.POST['address']
+    phone = request.POST['phone']
 
-	return render(request, 'dashboard_external_user.html')
+    return render(request, 'dashboard_external_user.html')
 
 
 # Show complete transaction history
@@ -106,26 +106,51 @@ def edit_user_details(request):
 @login_required()
 @permission_required('BankingSystem.user_operations', raise_exception=True)
 def passbook(request):  # done
-	accounts = request.user.profile.account_set.all()
-	account_transactions = []
-	for i in accounts:
-		account_transactions.extend(map(lambda x: str(x).split(), list(i.from_account.all())))
-		account_transactions.extend(map(lambda x: str(x).split(), list(i.to_account.all())))
+    accounts = request.user.profile.account_set.all()
+    account_transactions = []
+    for i in accounts:
+        account_transactions.extend(map(lambda x: str(x).split(), list(i.from_account.all())))
+        account_transactions.extend(map(lambda x: str(x).split(), list(i.to_account.all())))
 
-	# list of objects containing transaction_id , status , amount
-	# need account number for getting transactions
-	fields = {
-		'username': request.user.username,
-		'error': '',
-		'account_transactions': account_transactions,
-		'has_perm_user_operations': request.user.has_perm('BankingSystem.user_operations'),
-		'has_perm_create_payments': request.user.has_perm('BankingSystem.create_payments'),
-	}
+    # list of objects containing transaction_id , status , amount
+    # need account number for getting transactions
+    fields = {
+        'username': request.user.username,
+        'error': '',
+        'account_transactions': account_transactions,
+        'has_perm_user_operations': request.user.has_perm('BankingSystem.user_operations'),
+        'has_perm_create_payments': request.user.has_perm('BankingSystem.create_payments'),
+    }
 
-	return render(request, 'passbook.html')
+    return render(request, 'passbook.html')
 
 
 # TODO team, palash: Make html, fields with employee ID page and redirect to transaction confirmation OTP page.
 # Page created
 def debit_credit(request):
-	return None
+    fields = {
+        'accounts': request.user.profile.account_set.all(),
+        'username': request.user.username,
+        'error': '',
+    }
+    if request.method != 'POST':
+        return render(request, 'debit_credit.html', fields)
+
+    account = do_get(request.POST, 'account')
+    type = do_get(request.POST, 'debit_or_credit')  # Whether Debit or Credit transaction
+    preferred_employee = do_get(request.POST, 'preferred_employee')
+    amount = do_get(request.POST, 'amount')
+    try:
+        if (type == "debit"):
+            transaction = Transactions.create(Transactions.TYPE_DEBIT, request.user, None,
+                                              account, amount)
+        elif (type == "credit"):
+            transaction = Transactions.create(Transactions.TYPE_CREDIT, request.user, account,
+                                              None, amount)
+        else:  # can this happen?
+            abc = None
+
+    except BankingParseException as e:
+        fields['error'] = e.message
+        return render(request, 'debit_credit.html', fields)
+    return redirect("transaction_confirmation", transaction_id=transaction.id)
